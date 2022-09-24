f4578977bfb919cf3787c2fc25ad1ad315c3db9e
yuriel-v <emberbec@gmail.com>
2022-09-24 06:02:06 -0300

    Add dynamic text test, extract strings from views

--- app/src/main/java/com/example/aps/MainActivity.java
+++ app/src/main/java/com/example/aps/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.aps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
@@ -13,6 +16,41 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addDynamicText();
+    }
+
+    private void addDynamicText()
+    {
+        EditText field1 = findViewById(R.id.teste3);
+        TextView one = findViewById(R.id.teste1);
+        TextView two = findViewById(R.id.teste2);
+
+        field1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.length() > 0)
+                {
+                    Double value = Double.parseDouble(charSequence.toString());
+                    one.setText(String.format("%.2f", value * 2));
+                    two.setText(String.format("%.2f", value * value));
+                }
+                else
+                {
+                    one.setText("< *2 >");
+                    two.setText("< ^2 >");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     public void translate(View view)
