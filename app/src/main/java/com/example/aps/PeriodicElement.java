e1a34ea5f88d840de39a538d35928c280e1718ad
yuriel-v <emberbec@gmail.com>
2022-11-14 08:27:53 -0300

    Add elements list and class, add YAML reader

---
+++ app/src/main/java/com/example/aps/PeriodicElement.java
@@ -0,0 +1,13 @@
+package com.example.aps;
+
+public class PeriodicElement
+{
+    private int number;
+    private String symbol;
+    private String name;
+    private String mass;
+    private Object meltingPoint;  // int or string
+    private Object boilingPoint;
+
+    public PeriodicElement() { }
+}
